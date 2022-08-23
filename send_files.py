5caadc4971a341ba8f6a2dac380db16d5a5ece30
Tawata <40117806+T-S1@users.noreply.github.com>
2022-08-23 18:02:49 +0900

    Add files via upload

---
+++ send_files.py
@@ -0,0 +1,30 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "dir_path",
+    help="shared directory path",
+    type=str
+)
+args = parser.parse_args()
+
+from flask import Flask, send_file
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<html><h3>実習データ</h3><a href='/export/share_data'>ダウンロード</a></html>"
+
+
+@app.route("/export/share_data")
+def export_action_raspi():
+    return send_file(
+        args.dir_path,
+        as_attachment=True
+    )
+
+
+if __name__ == "__main__":
+    print(app.url_map)
+    app.run(debug=True,host='0.0.0.0', port=80)
